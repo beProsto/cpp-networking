66bc554a2f814f18801553668de66bf80e4f196d
beProsto <lavionperavion2@gmail.com>
2022-05-08 18:52:44 +0200

    serve

--- networking.hpp
+++ networking.hpp
@@ -1,22 +1,20 @@
 #pragma once
 
 #include <stdio.h>
-
+#include <assert.h>
 #include <iostream>
 
 #ifdef _WIN32
-
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
-#include <stdio.h>
-
 #endif
+
 
 #define SERVER_PORT 2137
 
 #define ERROR_OUT(x) { \
-		perror(x); \
+		printf("EXIT ERROR: %s\n", x); \
 		std::cin.get(); \
-		return 1; }
+		assert(false); }
--- server.cpp
+++ server.cpp
@@ -6,10 +6,10 @@ int main() {
 	// Init
 	WSAData wsaData;
 	if(WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-		ERROR_OUT("Couldn't start up WSA!\n");
+		ERROR_OUT("Couldn't start up WSA!");
 	}
 
-	
+	ERROR_OUT("WHAT");
 
 	std::cin.get();
 }
